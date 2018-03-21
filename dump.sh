959d7467e8c352874cc8dcde456e037324be7bfc
Petr <petr@kle.cz>
2018-03-21 18:18:20 +0100

    Oprava sk slovníku

--- dump.sh
+++ dump.sh
@@ -6,7 +6,7 @@ spelldump dict-cs-2.0/cs_CZ.dic dict-cs-2.0/cs_CZ.aff wordforms-cz.txt
 spelldump dict-sk/sk_SK/sk_SK.dic dict-sk/sk_SK/sk_SK.aff wordforms-sk.txt
 
 iconv -f ISO-8859-2 -t ASCII//TRANSLIT wordforms-cz.txt | tr '[:upper:]' '[:lower:]' | sort | uniq > wordforms-cz.txt.tmp
-cat wordforms-sk.txt | tr '[:upper:]' '[:lower:]' | sort | uniq > wordforms-sk.txt.tmp
+iconv -f UTF-8 -t ASCII//TRANSLIT wordforms-sk.txt | tr '[:upper:]' '[:lower:]' | sort | uniq > wordforms-sk.txt.tmp
 iconv -f ISO-8859-1 -t ASCII//TRANSLIT wordforms-en.txt | tr '[:upper:]' '[:lower:]' | sort | uniq > wordforms-en.txt.tmp
 iconv -f ISO-8859-1 -t ASCII//TRANSLIT wordforms-en-names.txt | tr '[:upper:]' '[:lower:]' | sort | uniq > wordforms-en-names.txt.tmp
 
